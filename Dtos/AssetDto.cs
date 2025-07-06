e4f98f31ad5cc40aeb06a2e3770321c2c03ba76d
Luciano Munhoz Silva <luciano.asimov@gmail.com>
2025-07-06 18:59:19 -0300

    First commit

---
+++ Dtos/AssetDto.cs
@@ -0,0 +1,9 @@
+﻿namespace radar_api.Dtos
+{
+    public class AssetDto
+    {
+        public string Ticker { get; set; }
+        public string UrlCode { get; set; }
+        public decimal TargetVariation { get; set; }
+    }
+}
---
+++ Models/Asset.cs
@@ -0,0 +1,10 @@
+﻿namespace radar_api.Models
+{
+    public class Asset
+    {
+        public int Id { get; set; }
+        public string Ticker { get; set; }
+        public string UrlCode { get; set; }
+        public decimal TargetVariation { get; set; }
+    }
+}
